d7450e6629cfac06e8a4ebec081729987c94a14f
Musfique Ahmed <anikmushfik@gmail.com>
2024-12-09 22:21:19 +0600

    Update prac.py

--- prac.py
+++ prac.py
@@ -1,2 +1,18 @@
 n = int(input())
+laptops = []
 
+for i in range(n):
+    price, quality = map(int, input().split())
+    laptops.append((price, quality))
+
+laptops.sort()
+alex_happy = True
+
+for j in range(1,n):
+    if laptops[j][1] > laptops[j-1][1]:
+        alex_happy = False
+
+if alex_happy:
+    print("Happy Alex")
+else:
+    print("Poor Alex")
